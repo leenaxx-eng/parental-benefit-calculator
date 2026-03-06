c70247ec1ee83da82a2c38931d1fb1fb58769472
Magdaleena <magdaleen2601@gmail.com>
2026-03-06 19:25:51 +0200

    Setup Spring Boot backend with SQLite configuration

---
+++ src/main/java/com/helmes/parental_benefit_calculator/ParentalBenefitCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.helmes.parental_benefit_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentalBenefitCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentalBenefitCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helmes/parental_benefit_calculator/ParentalBenefitCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helmes.parental_benefit_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentalBenefitCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
